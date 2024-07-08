8e3e90958059b9b6b965118d0815e38d4fdf56aa
Abigail Acuña <acuaabigail@yahoo.com>
2024-07-08 11:56:17 -0600

    Agregar archivos de proyecto.

---
+++ Persona/Models/PersonasDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persona.Models
+{
+    public class PersonasDBContext: DbContext
+    {
+
+        public PersonasDBContext(DbContextOptions options) : base(options)
+        { 
+        }
+
+        public DbSet<Persona> Personas { get; set; }
+
+    }
+}
